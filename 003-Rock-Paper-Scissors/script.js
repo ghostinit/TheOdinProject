392b61b9e1029c7ae54e9976731c78824c9edb2e
Adam Lyon <adam@nullrider.com>
2025-05-30 13:16:04 -0600

    Basic logic completed
    The script now correctly chooses a random value for the computer and figures out who won

--- 003-Rock-Paper-Scissors/script.js
+++ 003-Rock-Paper-Scissors/script.js
@@ -4,15 +4,47 @@ const rockButton = document.querySelector(".buttonRock");
 const paperButton = document.querySelector(".buttonPaper");
 const scissorButton = document.querySelector(".buttonScissors");
 
+const computerChoiceText = document.querySelector(".computerChoiceText");
+const outcomeText = document.querySelector(".outcome");
+
 const options = ["rock", "paper", "scissors"];
+const outcomes = ["You Win", "Computer Wins", "Tie"]
 
 let clickChoice;
+let outcome;
+
+let playerScore = 0;
+let computerScore = 0;
 
 function decideWinner() {
     console.log("User clicked " + clickChoice);
     
     console.log("Deciding winner");
     let computerChoice = options[Math.floor(Math.random() * 3)];
+    computerChoiceText.textContent = "The computer chose: " + computerChoice;
+
+    if (computerChoice === clickChoice) {
+        outcome = outcomes[2];
+    } else if (computerChoice === "rock") {
+        if (clickChoice === "paper") {
+            outcome = outcomes[0];
+        } else {
+            outcome = outcomes[1];
+        }
+    } else if (computerChoice === "paper") {
+        if (clickChoice === "scissors") {
+            outcome = outcomes[0];
+        } else {
+            outcome = outcomes[1];
+        }
+    } else if (computerChoice === "scissors") {
+        if (clickChoice === "rock"){
+            outcome = outcomes[0];
+        } else {
+            outcome = outcomes[1];
+        }
+    }
+    outcomeText.textContent = outcome;
 }
 
 function rockClick() {
@@ -23,7 +55,7 @@ rockButton.addEventListener("click", rockClick);
 
 function paperClick() {
     clickChoice = options[1];
-    decideWinner()
+    decideWinner();
 }
 paperButton.addEventListener("click", paperClick);
 
@@ -31,4 +63,4 @@ function scissorClick() {
     clickChoice = options[2];
     decideWinner();
 }
-scissorButton.addEventListener("click", scissorButton);
+scissorButton.addEventListener("click", scissorClick);
