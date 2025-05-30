4ba40615111eecb568b268614353acd884be0dc3
Adam Lyon <adam@nullrider.com>
2025-05-30 14:01:28 -0600

    Project completed
    I was tempted to add a lot more, but per the recommendation of TOP I will move on!

--- 003-Rock-Paper-Scissors/script.js
+++ 003-Rock-Paper-Scissors/script.js
@@ -7,14 +7,36 @@ const scissorButton = document.querySelector(".buttonScissors");
 const computerChoiceText = document.querySelector(".computerChoiceText");
 const outcomeText = document.querySelector(".outcome");
 
+const playerScoreText = document.querySelector(".playerScoreText");
+const computerScoreText = document.querySelector(".computerScoreText");
+
+const finalOutcomeMessage = document.querySelector(".finalOutcomeMessage");
+
+const resetButton = document.querySelector(".resetButton");
+
+const finalOutcomeDiv = document.querySelector(".finalOutcome");
+
 const options = ["rock", "paper", "scissors"];
 const outcomes = ["You Win", "Computer Wins", "Tie"]
 
+const maxScore = 10;
+
+let gameOver = false;
+
 let clickChoice;
 let outcome;
+let winner;
 
 let playerScore = 0;
 let computerScore = 0;
+
+finalOutcomeDiv.style.display = "none";
+
+function doGameOver() {
+    finalOutcomeDiv.style.display = "flex";
+    gameOver = true;
+    finalOutcomeMessage.textContent = winner;
+}
 
 function decideWinner() {
     console.log("User clicked " + clickChoice);
@@ -22,45 +44,82 @@ function decideWinner() {
     console.log("Deciding winner");
     let computerChoice = options[Math.floor(Math.random() * 3)];
     computerChoiceText.textContent = "The computer chose: " + computerChoice;
+    let intOutcome
 
     if (computerChoice === clickChoice) {
-        outcome = outcomes[2];
+        intOutcome = 2;
     } else if (computerChoice === "rock") {
         if (clickChoice === "paper") {
-            outcome = outcomes[0];
+            intOutcome = 0; // Player Wins
         } else {
-            outcome = outcomes[1];
+            intOutcome = 1; // Computer Wins
         }
     } else if (computerChoice === "paper") {
         if (clickChoice === "scissors") {
-            outcome = outcomes[0];
+            intOutcome = 0; // Player Wins
         } else {
-            outcome = outcomes[1];
+            intOutcome = 1; // Computer WinsresetButton
         }
     } else if (computerChoice === "scissors") {
-        if (clickChoice === "rock"){
-            outcome = outcomes[0];
+        if (clickChoice === "rock") {
+            intOutcome = 0; // Player Wins
         } else {
-            outcome = outcomes[1];
+            intOutcome = 1; // Computer Wins
         }
     }
+    
+    if (intOutcome === 0) { //Player wins
+        playerScore++;
+    }
+    else if (intOutcome === 1) { // Computer wins
+        computerScore++;
+    }
+    outcome = outcomes[intOutcome];
     outcomeText.textContent = outcome;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+
+    if (playerScore === maxScore) {
+        winner = "You Won!";
+        doGameOver();
+    } else if (computerScore === maxScore) {
+        winner = "Computer Won!"
+        doGameOver();
+    }
 }
 
 function rockClick() {
-    clickChoice = options[0];
-    decideWinner();
+    if (!gameOver) {
+        clickChoice = options[0];
+        decideWinner();
+    }
 }
 rockButton.addEventListener("click", rockClick);
 
 function paperClick() {
-    clickChoice = options[1];
-    decideWinner();
+    if (!gameOver) {
+        clickChoice = options[1];
+        decideWinner();
+    }
 }
 paperButton.addEventListener("click", paperClick);
 
 function scissorClick() {
-    clickChoice = options[2];
-    decideWinner();
+    if (!gameOver) {
+        clickChoice = options[2];
+        decideWinner();
+    }
 }
 scissorButton.addEventListener("click", scissorClick);
+
+function resetButtonClick() {
+    if (gameOver) {
+        playerScore = 0;
+        computerScore = 0;
+        gameOver = false;
+        playerScoreText.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
+        finalOutcomeDiv.style.display = "none";
+    }
+}
+resetButton.addEventListener("click", resetButtonClick);
