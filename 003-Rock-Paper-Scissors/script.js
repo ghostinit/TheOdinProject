f48f3930e35dda7e2df791b346929c9465252557
Adam Lyon <adam@nullrider.com>
2025-05-30 12:58:31 -0600

    Start Rock, Paper, Scissors Project
    Decided to do a very basic GUI.  Gui started and logic in place to click buttons and have the script respond

---
+++ 003-Rock-Paper-Scissors/script.js
@@ -0,0 +1,33 @@
+console.log("Welcome to Rock, Paper, Scissors!");
+
+const rockButton = document.querySelector(".buttonRock");
+const paperButton = document.querySelector(".buttonPaper");
+const scissorButton = document.querySelector(".buttonScissors");
+
+const options = ["rock", "paper", "scissors"];
+
+let clickChoice;
+
+function decideWinner() {
+    console.log("User clicked " + clickChoice);
+    
+    console.log("Deciding winner");
+    let computerChoice = options[Math.floor(Math.random() * 3)];
+}
+
+function rockClick() {
+    clickChoice = options[0];
+    decideWinner();
+}
+rockButton.addEventListener("click", rockClick);
+
+function paperClick() {
+    clickChoice = options[1];
+    decideWinner()
+}
+paperButton.addEventListener("click", paperClick);
+
+function scissorClick() {
+    clickChoice = options[2];
+    decideWinner();
+}
